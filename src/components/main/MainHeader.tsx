05d0b0ed5a267839f4142d973ae20c753f96684d
mzolkiewski <zolkiewski.marcin@gmail.com>
2021-09-20 15:02:05 +0200

    fixed waifu grid styling

--- src/components/main/MainHeader.tsx
+++ src/components/main/MainHeader.tsx
@@ -25,202 +25,204 @@ export default function MainHeader() {
   return (
     <Layout>
       <AppHeader />
-      <CustomContent id="upload">
+      <CustomContent>
         <Content>
-          <div className="grid-layout">
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie1.jpg'} />
+          <div className="grid-wrapper">
+            <div className="grid-layout">
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie1.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu1.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu1.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie2.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu2.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie2.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie3.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu3.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu2.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie4.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu4.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie3.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie5.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu5.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu3.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie6.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu6.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie4.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie7.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu7.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu4.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie8.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu8.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie5.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie9.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu9.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu5.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie10.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu10.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie6.jpg'} />
+              <div className="grid-item span-1">
+                <ImageUploader onUploadDone={handleSelfieUploadDone} />
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu6.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie11.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu11.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie7.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie12.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu12.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu7.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie13.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu13.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie8.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie14.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu14.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu8.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie15.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu15.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie9.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie16.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu16.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu9.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie17.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu17.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie10.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie18.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu18.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu10.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie19.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu19.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-1">
-              <ImageUploader onUploadDone={handleSelfieUploadDone} />
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie11.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie20.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu20.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu11.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie21.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu21.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie12.jpg'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie22.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu22.png'} />
+                </div>
               </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu12.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie23.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu23.png'} />
+                </div>
               </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie13.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu13.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie14.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu14.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie15.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu15.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie16.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu16.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie17.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu17.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie18.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu18.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie19.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu19.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie20.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu20.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie21.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu21.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie22.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu22.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie23.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu23.png'} />
-              </div>
-            </div>
-            <div className="grid-item span-2">
-              <div className="selfie">
-                <Image width={160} preview={false} src={'../img/selfie/selfie24.jpg'} />
-              </div>
-              <div className="waifu">
-                <Image width={160} preview={false} src={'../img/waifu/waifu24.png'} />
+              <div className="grid-item span-2">
+                <div className="selfie">
+                  <Image width={160} preview={false} src={'../img/selfie/selfie24.jpg'} />
+                </div>
+                <div className="waifu">
+                  <Image width={160} preview={false} src={'../img/waifu/waifu24.png'} />
+                </div>
               </div>
             </div>
           </div>
@@ -238,8 +240,16 @@ const CustomContent = styled.div`
   .titleRed {
     color: ${flamingo};
   }
+  .grid-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   .grid-layout {
+    flex-grow: 0;
+    flex-shrink: 0;
     display: grid;
+    width: 1280px;
     grid-template-columns: repeat(auto-fill, minmax(8em, auto));
     grid-auto-rows: minmax(8em, auto);
     grid-gap: 0px;
